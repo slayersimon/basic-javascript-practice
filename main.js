c0bbb08d18baeebfc7a5a144bdf79e779a7826a4
Simon McFarland <10801276@cs715b-22.ad.uvu.edu>
2019-10-08 12:22:27 -0600

    added starwars things

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log('I am JavaScript funning in your page!')
+
+let mainHeader = document.querySelector('h1')
+
+console.log(mainHeader)
+
+let allHeaders = document.querySelectorAll('h1')
+
+console.log(allHeaders)
+
+mainHeader.textContent = 'Simon Rulz'
+
+mainHeader.setAttribute("Style", "color: red; border: 1px solid blue;")
+
+let newDiv = document.createElement("div");
+newDiv.textContent = 'Here I am! A new Div!'
+
+document.querySelector('main').setAttribute("style", "background-color: #dfdfdf;")
+document.querySelector('main').appendChild(newDiv)
