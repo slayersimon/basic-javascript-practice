741d7d1d9ee7092d4bfe5c31eaa7b1fdcede8d94
Simon McFarland <10801276@cs715b-22.ad.uvu.edu>
2019-10-29 12:41:36 -0600

    Broke pokemon?

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,17 +1,40 @@
 
-async function getPokemonData(url) {
+/*async function getPokemonData(url) {
 const response = await fetch(url)
 return await response.json()
-}
+}*/
 
-async function getTheData() {
-
-
+async function getPokemonData(url) {
 try {
-    const data = await getPokemonData('https://pokeapi.co/api/v2/pokemon')
+    const response = await getPokemonData()
+    const data = await Response.json()
+    console.log(data)
 } catch (error) {
     console.log(error)
 }
 }
 
-getTheData()
+const data = getPokemonData('https://pokeapi.co/api/v2/pokemon')
+
+
+function populateDOM(pokeArray) {
+pokeArray.forEach((pokemon) => {
+    let personDiv = document.createElement('div', 'charDiv')
+    let name = document.createElement('h3')
+    let gender = document.createElement('p')
+    let pic = document.createElement('img', 'picDiv')
+
+    /*let charNum = getCharNumber(person.url)*/
+ 
+    name.textContent = person.name
+    gender.textContent = `Gender: ${person.gender}`
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+    
+       mainArea.appendChild(personDiv)
+   
+       personDiv.setAttribute('class', 'charDiv')
+   }
