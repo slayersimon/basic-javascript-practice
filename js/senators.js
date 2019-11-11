3584715fd8185d7452087ef5921e3fc9b3313968
Simon <10801276@my.uvu.edu>
2019-11-11 14:02:27 -0800

    started adding code to senator section

--- js/senators.js
+++ js/senators.js
@@ -11,5 +11,19 @@ async function getAPIData(url) {
 // now, use the returned async data
 let allSenators = []
 const theData = getAPIData('senators.json').then(data => {
-    console.log(data)
+    allSenators = data.results[0].members
 })
+
+const container = document.querySelector('.container')
+
+function populateDOM(senator_array) {
+    let card = document.createElement('div') 
+    card.setAttribute('class', 'card')
+    let cardImage = document.createElement('div')
+    cardImage.setAttribute('class', 'card-image')
+    let figure = document.createElement('figure')
+    figure.setAttribute('class', 'image is-4by3')
+    let figureImage = document.createElement('img')
+    figureImage.src = "https://bulma.io/images/placeholders/1280x960.png"
+    figureImage.alt = "Placeholder image"
+}
