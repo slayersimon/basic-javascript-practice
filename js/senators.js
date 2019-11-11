0411ba5f3df8b6f87954d4a5326a92f3306daeea
Simon <10801276@my.uvu.edu>
2019-11-11 14:42:35 -0800

    added card content for senators

--- js/senators.js
+++ js/senators.js
@@ -2,7 +2,7 @@ async function getAPIData(url) {
     try {
         const response = await fetch(url)
         const data = await response.json()
-        return data 
+        return data
     } catch (error) {
         console.error(error)
     }
@@ -12,18 +12,58 @@ async function getAPIData(url) {
 let allSenators = []
 const theData = getAPIData('senators.json').then(data => {
     allSenators = data.results[0].members
+    populateDOM(allSenators)
 })
 
 const container = document.querySelector('.container')
 
 function populateDOM(senator_array) {
-    let card = document.createElement('div') 
-    card.setAttribute('class', 'card')
-    let cardImage = document.createElement('div')
-    cardImage.setAttribute('class', 'card-image')
+    senator_array.foreach(senator => {
+        let card = document.createElement('div')
+        card.setAttribute('class', 'card')
+        let cardImage = document.createElement('div')
+        cardImage.setAttribute('class', 'card-image')
+        let figure = document.createElement('figure')
+        figure.setAttribute('class', 'image')
+        let figureImage = document.createElement('img')
+        figureImage.src = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}_200jpg`
+        figureImage.alt = "Placeholder image"
+
+        figure.appendChild(figureImage)
+        cardImage.appendChild(figure)
+        card.appendChild(cardImage)
+        card.appendChild(cardContent(senator))
+        container.appendChild(card)
+    })
+}
+
+function cardContent(senator) {
+    let cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'card-content')
+    let media = document.createElement('div')
+    media.setAttribute('class', 'media')
+    let mediaLeft = document.createElement('div')
+    mediaLeft.setAttribute('class', 'media-left')
     let figure = document.createElement('figure')
-    figure.setAttribute('class', 'image is-4by3')
-    let figureImage = document.createElement('img')
-    figureImage.src = "https://bulma.io/images/placeholders/1280x960.png"
-    figureImage.alt = "Placeholder image"
+    figure.setAttribute('class', 'image is-48x48')
+    let img = document.createElement('img')
+    img.src = `https://bulma.io/images/placeholders/96x96.png`
+    img.alt = 'Placeholder image'
+    let mediaContent = document.createElement('div')
+    mediaContent.setAttribute('class', 'media-content')
+    let titleP = document.createElement('p')
+    titleP.setAttribute('class', 'title is-4')
+    titleP.textContent = `foo`
+    let subtitleP = document.createElement('p')
+    subtitleP.setAttribute('class', 'subtitle is-6')
+    subtitleP.textContent = 'bar' 
+
+    mediaContent.appendChild(titleP)
+    mediaContent.appendChild(subtitleP)
+    figure.appendChild(img)
+    mediaLeft.appendChild(figure)
+    media.appendChild(mediaLeft)
+    media.appendChild(mediaContent)
+    cardContent.appendChild(media)
+    return cardContent
 }
